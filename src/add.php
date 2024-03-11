46cbfb810e6e3cf572c71c5e81de53a61f4c8245
AnderPR <ander.pelayoremesal@iemsmiguelherrero.com>
2024-03-11 23:10:52 +0100

    modificamos ficheros

--- src/add.php
+++ src/add.php
@@ -1,101 +1,52 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">	
-	<title>Alta de material</title>
-<!--	
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
--->	
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
+    <title>Alta de material</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-<!--	
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--->
-<div>
-	<header>
-		<h1>Panel de Control</h1>
-	</header>
+<div class="container">
+    <header>
+        <h1 class="mt-5">Panel de Control</h1>
+    </header>
 
-	<main>
+    <main class="mt-4">                
+        <?php
+        include_once("config.php");
 
-<?php
-//Incluye fichero con parámetros de conexión a la base de datos
-include_once("config.php");
+        if(isset($_POST['inserta'])) 
+        {
+            $id = mysqli_real_escape_string($mysqli, $_POST['id']);
+            $marca = mysqli_real_escape_string($mysqli, $_POST['marca']);
+            $tipo_prenda = mysqli_real_escape_string($mysqli, $_POST['tipo_prenda']);
+            $en_stock = mysqli_real_escape_string($mysqli, $_POST['en_stock']);
+            $precio = mysqli_real_escape_string($mysqli, $_POST['precio']);
+            $material_reciclado = mysqli_real_escape_string($mysqli, $_POST['material_reciclado']);
 
-/*Comprueba si hemos llegado a esta página PHP a través del formulario de altas. 
-En este caso comprueba la información "inserta" procedente del botón Agregar del formulario de altas
-Transacción de datos utilizando el método: POST
-*/
-if(isset($_POST['inserta'])) 
-{
-//Obtiene los datos (name, surname y age) a partir del formulario de alta por el método POST (Se envía a través del body del HTTP Request. No aparece en la URL)
-	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
-	$marca = mysqli_real_escape_string($mysqli, $_POST['marca']);
-	$tipo_prenda = mysqli_real_escape_string($mysqli, $_POST['tipo_prenda']);
-	$en_stock = mysqli_real_escape_string($mysqli, $_POST['en_stock']);
-	$precio = mysqli_real_escape_string($mysqli, $_POST['precio']);
-	$material_reciclado = mysqli_real_escape_string($mysqli, $_POST['material_reciclado']);
-/*Con mysqli_real_scape_string protege caracteres especiales en una cadena para ser usada en una sentencia SQL.
-Esta función es usada para crear una cadena SQL legal que se puede usar en una sentencia SQL. 
-Los caracteres codificados son NUL (ASCII 0), \n, \r, \, ', ", y Control-Z.*/
+            if(empty($marca) || empty($tipo_prenda) || empty($en_stock) || empty($precio) || empty($material_reciclado)) 
+            {
+                echo "<div class='alert alert-danger' role='alert'>Por favor, complete todos los campos.</div>";
+            } else {
+                $stmt = mysqli_prepare($mysqli, "INSERT INTO MaterialTrailRunning (marca, tipo_prenda, en_stock, precio, material_reciclado) VALUES (?, ?, ?, ?, ?)");
+                mysqli_stmt_bind_param($stmt, "ssdds", $marca, $tipo_prenda, $en_stock, $precio, $material_reciclado);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_free_result($stmt);
+                mysqli_stmt_close($stmt);
 
-//Comprueba si existen campos vacíos
-	if(empty($marca) || empty($tipo_prenda) || empty($en_stock) || empty($precio) || empty($material_reciclado)) 
-	{
-		if(empty($marca)) {
-			echo "<font color='red'>Campo nombre vacío.</font><br/>";
-		}
+                echo "<div class='alert alert-success' role='alert'>Datos añadidos correctamente</div>";
+                echo "<a href='index.php' class='btn btn-primary'>Ver resultado</a>";
+            }
+        }
 
-		if(empty($tipo_prenda)) {
-			echo "<font color='red'>Campo apellido vacío.</font><br/>";
-		}
-
-		if(empty($en_stock)) {
-			echo "<font color='red'>Campo edad vacío.</font><br/>";
-		}
-		
-		if(empty($precio)) {
-			echo "<font color='red'>Campo edad vacío.</font><br/>";
-		}
-		
-		if(empty($material_reciclado)) {
-			echo "<font color='red'>Campo edad vacío.</font><br/>";
-		}
-//Enlace a la página anterior
-		echo "<a href='javascript:self.history.back();'>Volver atras</a>";
-	} //fin si
-	else 
-	{
-//Prepara una sentencia SQL para su ejecución. En este caso el alta de un registro de la BD.		
-		$stmt = mysqli_prepare($mysqli, "INSERT INTO MaterialTrailRunning (marca,tipo_prenda,en_stock,precio,material_reciclado) VALUES(?,?,?,?,?)");
-/*Enlaza variables como parámetros a una setencia preparada. 
-i: La variable correspondiente tiene tipo entero
-d: La variable correspondiente tiene tipo doble
-s:	La variable correspondiente tiene tipo cadena
-*/		
-		mysqli_stmt_bind_param($stmt, "ssiis", $marca, $tipo_prenda, $en_stock, $precio, $material_reciclado);
-//Ejecuta una consulta preparada		
-		mysqli_stmt_execute( $stmt);
-//Libera la memoria donde se almacenó el resultado		
-		mysqli_stmt_free_result($stmt);
-//Cierra la sentencia preparada		
-		mysqli_stmt_close($stmt);
-//Muestra mensaje exitoso		
-		echo "<div>Datos añadidos correctamente</div>";
-		echo "<a href='index.php'>Ver resultado</a>";
-	}//fin sino
-}
-
-//Cierra la conexión
-mysqli_close($mysqli);
-?>
-
-	</main>
-	<footer>
-    Created by the IES Miguel Herrero team &copy; 2024
-  	</footer>
+        mysqli_close($mysqli);
+        ?>
+    </main>
+    <footer class="mt-5">
+        <p>Created by the IES Miguel Herrero team &copy; 2024</p>
+    </footer>
 </div>
 </body>
 </html>
--- src/index.php
+++ src/index.php
@@ -1,93 +1,145 @@
 <?php
-/*Incluye parámetros de conexión a la base de datos: 
-DB_HOST: Nombre o dirección del gestor de BD MariaDB
-DB_NAME: Nombre de la BD
-DB_USER: Usuario de la BD
-DB_PASSWORD: Contraseña del usuario e la BD
-*/
-
 include_once("config.php");
 
-//Consulta de selección. Selecciona todos los usuarios ordenados de manera descendente por el campo id
 $result = mysqli_query($mysqli, "SELECT * FROM MaterialTrailRunning ORDER BY id ASC");
-
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">	
-	<title>Panel de control</title>
-<!--	
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
--->	
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
+    <title>Panel de control</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+    <style>
+        .page-item.active .page-link {
+            background-color: #007bff !important;
+            border-color: #007bff !important;
+        }
+		.container-table {
+            max-width: 800px;
+            margin: auto;
+        }
+    </style>
 </head>
-<body>
-<!--	
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--->	
-<div>
-	<header>
-		<h1>Panel de Control</h1>
-	</header>
-
-	<main>
-	<ul>
-		<li><a href="index.php">Inicio</a></li>
-		<li><a href="add.html">Alta</a></li>
-	</ul>
-	<h2>Listado de material</h2>
-	<table border="1">
-	<thead>
-		<tr>
-			<th>Codigo</th>
-			<th>Marca</th>
-			<th>Tipo de prenda</th>
-			<th>Stock</th>
-			<th>Precio</th>
-			<th>Reciclado o NO</th>
-		</tr>
-	</thead>
-	<tbdody>
-<?php
-/*mysqli_fetch_array- Busca una fila de una consulta y devuelve un array asociativo
-ARRAYS CON ÍNDICE
-$productos = array();
-$productos[0] = "Disco SSD";
-$productos[1] = "Memoria RAM";
-$productos[2] = "Monitor";
-ARRAYS ASOCIATIVO:
-$res["id"] = "7";
-$res["name"] = "Pedro";
-$res["surname"] = "Zapata";
-$res["age"] = "23";
-*/
-//Genera la tabla de la página inicial
-	while($res = mysqli_fetch_array($result)) {
-		echo "<tr>\n";
-		echo "<td>".$res['id']."</td>\n";
-		echo "<td>".$res['marca']."</td>\n";
-		echo "<td>".$res['tipo_prenda']."</td>\n";
-		echo "<td>".$res['en_stock']."</td>\n";
-		echo "<td>".$res['precio']."</td>\n";
-		echo "<td>".$res['material_reciclado']."</td>\n";
-		echo "<td>";
-//En la última columna se añader dos enlaces para editar y modificar el registro correspondiente. Se le pasa por el método GET el id del registro		
-		echo "<a href=\"edit.php?id=$res[id]\">Editar</a>\n";
-		echo "<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('¿Está segur@ que desea eliminar el registro?')\" >Eliminar</a></td>\n";
-		echo "</td>";
-		echo "</tr>\n";
-	}
-//Cierra la conexión de BD previamente abierta
-	mysqli_close($mysqli);
-	?>
-	</tbdody>
-	</table>
-	</main>
-	<footer>
-    Created by the IES Miguel Herrero team &copy; 2024
-  	</footer>
+<body id="page-top">
+<div id="wrapper">
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+            <div class="sidebar-brand-icon">
+                <i class="fas fa-chart-line"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">Panel de Control</div>
+        </a>
+        <hr class="sidebar-divider my-0">
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">Opciones</div>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Inicio</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="add.html">
+                <i class="fas fa-plus"></i>
+                <span>Alta</span>
+            </a>
+        </li>
+    </ul>
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <ul class="navbar-nav ml-auto">
+                    <div class="topbar-divider d-none d-sm-block"></div>
+                    <li class="nav-item dropdown no-arrow">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Usuario</span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Perfil
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Salir
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+            <div class="container-fluid">
+                <h1 class="h3 mb-4 text-gray-800">Listado de material</h1>
+                <div class="card shadow mb-4">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>Codigo</th>
+                                        <th>Marca</th>
+                                        <th>Tipo de prenda</th>
+                                        <th>Stock</th>
+                                        <th>Precio</th>
+                                        <th>Reciclado o NO</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>Codigo</th>
+                                        <th>Marca</th>
+                                        <th>Tipo de prenda</th>
+                                        <th>Stock</th>
+                                        <th>Precio</th>
+                                        <th>Reciclado o NO</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </tfoot>
+                                <tbody>
+                                    <?php
+                                        while($res = mysqli_fetch_array($result)) {
+                                            echo "<tr>\n";
+                                            echo "<td>".$res['id']."</td>\n";
+                                            echo "<td>".$res['marca']."</td>\n";
+                                            echo "<td>".$res['tipo_prenda']."</td>\n";
+                                            echo "<td>".$res['en_stock']."</td>\n";
+                                            echo "<td>".$res['precio']."</td>\n";
+                                            echo "<td>".$res['material_reciclado']."</td>\n";
+                                            echo "<td>
+                                                    <a href=\"edit.php?id=$res[id]\" class=\"btn btn-primary btn-sm\">Editar</a>
+                                                    <a href=\"delete.php?id=$res[id]\" class=\"btn btn-danger btn-sm\" onClick=\"return confirm('¿Está seguro que desea eliminar el registro?')\">Eliminar</a>
+                                                  </td>\n";
+                                            echo "</tr>\n";
+                                        }
+                                        mysqli_close($mysqli);
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <span>Created by the IES Miguel Herrero team &copy; 2024</span>
+            </div>
+        </footer>
+    </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#dataTable').DataTable();
+    });
+</script>
 </body>
 </html>
